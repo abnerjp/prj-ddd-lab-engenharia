65ee1ea62106280908f30ca30dbc87e1688b3d11
Abner J Pelisser <abner.pelisser@gmail.com>
2021-09-25 08:57:21 -0300

    Views de produtos criadas

---
+++ Web/Models/ProtudoViewlModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class ProtudoViewlModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        [Display(Name = "Preço")]
+        public double Preco { get; set; }
+    }
+}
+
